cb07d76ab28f252d7b3d0dbcfc2ee8e8e372aa28
mengen.dai <mengen.dai@outlook.com>
2025-02-19 20:06:24 +0800

    feat(src): 更新 index.tsx导出内容
    - 在 src/index.tsx 中导出了多个新类型和接口
    - 包括 Event、Resource、Checkpoint、Milestone 等相关类型
    - 以及各种 MountArg 和 ContextMenuArg 接口
    - 还有 Public*Api 类的导出

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,34 @@
-export {FullSchedule} from "./modules/full-schedule/index";
+export {
+    FullSchedule,
+    Event,
+    EventMountArg,
+    PublicEventApi,
+    EventMoveMountArg,
+    EventContextMenuArg,
+    EventResizeMountArg,
+    Resource,
+    PublicResourceApi,
+    ResourceAreaColumn,
+    ResourceLaneMountArg,
+    ResourceLabelMountArg,
+    ResourceLaneContextMenuArg,
+    ResourceLabelContextMenuArg,
+    Checkpoint,
+    CheckpointMountArg,
+    PublicCheckpointApi,
+    CheckpointMoveMountArg,
+    CheckpointContextMenuArg,
+    Milestone,
+    MilestoneStatus,
+    MilestoneMountArg,
+    PublicMilestoneApi,
+    MilestoneMoveMountArg,
+    MilestoneContextMenuArg,
+    ScheduleProps,
+    ScheduleMountArg,
+    PublicScheduleApi,
+    SelectInfoArg
+} from "./modules/full-schedule/index";
 export {FullColorPicker} from "./modules/full-color-picker/index";
 export {FullDraggableModal} from "./modules/full-draggable-modal";
 export {FullTreeAdjuster} from "./modules/full-tree-adjuster/index";
